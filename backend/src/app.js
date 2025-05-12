3fdf259207c16d3dbf44c76fff8a073a6a0e7d4d
bbdavidherrera <bherrerabarajs@gmail.com>
2025-05-11 22:59:46 -0500

    Render-BDHB-MNISG

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 /*importamos al framework express */
 
-import express from "express";
+import express, { request } from "express";
 import categoriasRoutes from "./routers/categorias.routes.js"
 import rollosRoutes from "./routers/rollos.routes.js"
 import usuariosRoutes from "./routers/usuarios.routes.js"
@@ -20,5 +20,10 @@ app.use("/api/categorias",categoriasRoutes)
 app.use("/api/rollos", rollosRoutes)
 
 app.use("/admin", usuariosRoutes)
+
+
+app.get("/",(req,res)=>{
+    res.send("Manuel Isaac Gomez Galvis y Herrera Barajas Brayan David B191")
+});
 
 export default app;
