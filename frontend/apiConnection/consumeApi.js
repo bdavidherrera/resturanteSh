3afbb31d28ebc5117bc168b6664cf19d68508994
bbdavidherrera <bherrerabarajs@gmail.com>
2025-05-18 10:54:08 -0500

    promociones-frontend-commint

--- frontend/apiConnection/consumeApi.js
+++ frontend/apiConnection/consumeApi.js
@@ -1,5 +1,6 @@
-const url = "http://localhost:8000/api/rollos"
-const urlC = "http://localhost:8000/api/categorias"
+const url = "https://iki-sudhi-pokes-brayan-herrera-manuel.onrender.com/api/rollos"
+const urlC = "https://iki-sudhi-pokes-brayan-herrera-manuel.onrender.com/api/categorias"
+const urlP = "https://iki-sudhi-pokes-brayan-herrera-manuel.onrender.com/api/Promociones"
 
 export const obtainRollos = async ()=>{
     try {
@@ -18,6 +19,16 @@ export const obtainCategorias = async ()=>{
         return categorias;
     } catch (error) {
         console.error("error al obtener las categorias");
+    }
+}
+
+export const obtainPromociones = async ()=>{
+    try {
+        const resultadoPromociones = await fetch(urlP);
+        const Promociones = await resultadoPromociones.json();
+        return Promociones;
+    } catch (error) {
+        console.error("error al obtener las Promociones");
     }
 }
 
---
+++ frontend/js/promociones.js
@@ -0,0 +1,1 @@
+import { obtainPromociones } from "../apiConnection/consumeApi.js";
